81fa25ebb575da3090a6f38a1f83cdd5cb17deef
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-18 10:43:10 -0300

    firstCommit

---
+++ stack.c
@@ -0,0 +1,37 @@
+#include "stack.h"
+
+struct stackNode* push (struct stackNode* stack, int data) {
+    struct stackNode* newNode = (struct stackNode*) malloc (sizeof(struct stackNode));
+    newNode->data = data;
+    newNode->next = stack;
+    
+    return newNode;
+}
+
+int pop (struct stackNode** stack) {
+    if (*stack == NULL) {
+        printf("Error! The stack is empty");
+        return -1;
+    } else {
+        struct stackNode* tempNode = *stack;
+        int popedData = tempNode->data;
+
+        *stack = tempNode->next;
+        free(tempNode);
+
+        return popedData;
+    }
+}
+
+int peek (struct stackNode* stack) {
+    if (stack != NULL) return stack->data;
+    else {
+        printf("Error! The stack is empty");
+        return -1;
+    }
+}
+
+bool isEmpty (struct stackNode* stack) {
+    if (stack == NULL) return true;
+    else return false;
+}
---
+++ stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct stackNode {
+    int data;
+    struct stackNode* next;
+};
+
+struct stackNode* push (struct stackNode* stack, int data);
+int pop (struct stackNode** stack);
+int peek (struct stackNode* stack);
+bool isEmpty (struct stackNode* stack);
+
+#endif
